bc830748f8cb36a49e815b00a63860458b75f65e
huggyballs <spomich123@gmail.com>
2020-05-15 12:20:38 +0200

    Create apristup.py

---
+++ apristup.py
@@ -0,0 +1,14 @@
+#glavni program
+#upravljanje zaslonom i zvukom
+
+import lcddriver
+import RPi.GPIO as GPIO
+import time
+import os
+import sys
+from threading import Thread
+import threading
+import json
+from select import select
+import tkinter as tk
+from tkinter import ttk
