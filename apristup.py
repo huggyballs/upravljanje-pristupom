d7d99d61957802f2e5b72a879449456b4041f0e5
huggyballs <spomich123@gmail.com>
2020-05-29 15:05:10 +0200

    Update apristup.py

--- apristup.py
+++ apristup.py
@@ -3,21 +3,24 @@
 
 import lcddriver
 import RPi.GPIO as GPIO
+
 import time
+import thread
+
 import os
 import sys
-from threading import Thread
-import threading
-import json
-from select import select
-import tkinter as tk
-from tkinter import ttk
+import mysql
+import tty
+import termios
+import logging
+
+
+DEBUG=True
+VERNOSE=True
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
 buzzer = 5
 relay = 6
 GPIO.setup(buzzer,GPIO.OUT)
 GPIO.setup(relay,GPIO.OUT)
-ekran = lcddriver.lcd()
