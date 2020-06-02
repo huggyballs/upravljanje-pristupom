7a467fac1db22051129f55c4894abbc1fa3d8350
huggyballs <spomich123@gmail.com>
2020-06-02 14:07:28 +0200

    Update apristup.py

--- apristup.py
+++ apristup.py
@@ -18,9 +18,64 @@ import logging
 DEBUG=True
 VERNOSE=True
 
-GPIO.setmode(GPIO.BCM)
-
 buzzer = 5
 relay = 6
-GPIO.setup(buzzer,GPIO.OUT)
-GPIO.setup(relay,GPIO.OUT)
+
+def NFCReadAccess():
+    pass
+
+def NFCAddCheck():
+    pass
+
+def buzzerBeep():
+    GPIO.output(buzzer,GPIO.HIGH)
+    print("BIIIIP")
+    sleep(1)
+    GPIO.output(buzzer,GPIO.LOW)
+    print("KRAJ BIIIIP")
+    sleep(1)
+    pass
+
+def relayOpen():
+    GPIO.output(buzzer,GPIO.HIGH)
+    print("Otkljucano")
+    sleep(5)
+    GPIO.output(buzzer,GPIO.LOW)
+    print("Zakljucano")
+    pass
+
+def main():
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(buzzer,GPIO.OUT)
+        GPIO.setup(relay,GPIO.OUT)
+
+        display = lcddriver.lcd()
+
+        while True:
+            display.lcd_clear()
+            display.lcd_display_string("Unesite PIN:", 1)
+            unos = input()
+            if unos == 1 :
+                print("Pokusaj dodavanja korisnika")
+                NFCAddCheck()
+                pass
+            elif unos == 1234 :
+                print("Unesen tocan PIN")
+                NFCReadAccess()
+                pass
+            else:
+                print("Netocan unos")
+                display.lcd_clear()
+                display.lcd_display_string("Netocan unos!", 1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("Kraj rada programa")
+        pass    
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
