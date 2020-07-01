88034d2657050f2e5b1ad9cd152fdb8419e0bd62
huggyballs <spomich123@gmail.com>
2020-07-01 13:12:03 +0200

    Update apristup.py

--- apristup.py
+++ apristup.py
@@ -43,7 +43,7 @@ try:
     #ovo obrisati nakon prvog pokretanja
     #mycursor.execute("INSERT INTO Users (Seclev, role) VALUES (%s,%s)", (2, "original"))
     #ajdi = mycursor.lastrowid
-    mycursor.execute("INSERT INTO Devices (UserId, DeviceId)", (2, "Type2Tag ID=1ED3A622"))
+    mycursor.execute("INSERT INTO Devices (UserId, DeviceId) VALUES (%s,%s)", (2, "Type2Tag ID=1ED3A622"))
     pass
 except:
     pass
