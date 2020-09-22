f52928f93133a972cf6d7c7e9ad935afc2989dd6
huggyballs <spomich123@gmail.com>
2020-09-22 12:12:03 +0200

    Update prvo_dodavanje.py

--- prvo_dodavanje.py
+++ prvo_dodavanje.py
@@ -1,2 +1,48 @@
 import nfc
 import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="emovis",
+    passwd="emovis",
+    database="kontrolapristupa",
+    autocommit=True
+    )
+mycursor = db.cursor(buffered=True)
+
+try:
+    mycursor.execute("CREATE TABLE Users (id int PRIMARY KEY NOT NULL AUTO_INCREMENT, Seclev ENUM('1', '2') NOT NULL, role VARCHAR(10) NOT NULL)")
+    pass
+except:
+    print("postoji")
+    pass
+try:
+    mycursor.execute("CREATE TABLE Devices (DeviceNum int PRIMARY KEY NOT NULL AUTO_INCREMENT, UserId int NOT NULL, DeviceId VARCHAR(40) NOT NULL)")
+    pass
+except:
+    print("postoji")
+    pass
+
+clf = nfc.ContactlessFrontend()
+clf.open('ttyAMA0')
+
+try:
+    mycursor.execute("INSERT INTO Users (Seclev, role) VALUES (%s, %s)", (2, 'original'))
+    lstrow = mycursor.lastrowid
+    lstrow = int(lstrow)
+    try:
+        devID = clf.connect(rdwr={'on-connect': lambda tag: False})
+        devID = str(devID)
+        print(devID)
+        print("Uspjesno citanje!")
+
+        mycursor.execute("INSERT INTO Devices (UserId, DeviceId) VALUES (%s,%s)", (lstrow, devID))
+        pass
+    except:
+        print("Neuspjesno citanje")
+        pass
+except:
+    print("Neuspjesno dodavanje admina")
+    pass
+
+clf.close()
